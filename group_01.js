801d3c79839d48fcd43e5165343a8b190528e1e7
mat mccully <mccully1894@gmail.com>
2016-07-28 17:23:35 -0500

    second revision

--- group_01.js
+++ group_01.js
@@ -4,43 +4,74 @@ var boo = ["Boo", "11435", "54000", 3];
 var scout = ["Scout", "6243", "74750", 5];
 
 var employees = [atticus, jem, boo, scout];
-var bonus = [];
-var bonusA;
-var bonusB;
-var bonusC;
-var bonusD;
 
-var bonusFinder = function(){
-for(var i=0;i < employees.length; i++){
-  if(employees[i][3] <= 2){
-    var bonusA = "No bonus";
-    bonus.push(bonusA);
-  } else if (employees[i][3] == 3) {
-    if(years()){
-      bonus.push(bonusB);
+
+function calculateSTI(emp){
+    var name = emp[0];
+    var empId = empInfo[1];
+    var currentSalary = Math.round(parseFloat(emp[2]));
+    var rating = emp[3];
+
+    var bonusPercent = 0;
+    var bonusMoney = 0;
+    var processedEmployee = [];
+
+
+
+    var totalSalary = 0;
+
+    switch(rating){
+        case 0:
+        case 1:
+        case 2:
+            bonusPercent = 0;
+            break;
+        case 3:
+            bonusPercent = .04;
+            break;
+        case 4:
+            bonusPercent = .06;
+            break;
+        case 5:
+            bonusPercent = .10;
+            break;
+        default:
+            bonusPercent = 0;
     }
-    }else if(employees[i][1].length == 4){
-    var bonusB = (employees[i][2] * 1.04);
-    bonus.push(bonusB);
 
-  } else if (employees[i][3] == 4){
-    var bonusC = (employees[i][2] * 1.06);
-    bonus.push(bonusC);
-  } else if (employees[i][3] == 5){
-    var bonusD = (employees[i][2] * 1.10);
-    bonus.push(bonusD);
-  }
+    bonusPercent = adjustBonusPercent(empId, bonusPercent, currentSalary);
+    console.log(bonusPercent);
 
+    processedEmployee[0] = name;
+    processedEmployee[1] = bonusPercent;
 
+    bonusMoney = Math.round(bonusPercent * currentSalary);
+    totalSalary = bonusMoney + currentSalary;
+
+    processedEmployee[2] = totalSalary;
+    processedEmployee[3] = bonusMoney;
+
+    return processedEmployee;
 
 }
 
+function adjustBonusPercent(empId, bonusPercent, currentSalary){
+    console.log("first parameter check ", bonusPercent)
+    if(empNumber.length == 4){
+        bonusPercent += .05;
+        console.log(bonusPercent);
+    }
+
+    if(currentSalary > 65000){
+        bonusPercent -= .01;
+    }
+
+    if(bonusPercent > .13){
+        bonusPercent = .13;
+    }
+    return bonusPercent;
 }
-var years = function(){
-  for(var i =0; i < employees.length; i++){
-  if(employees[i][1].length ==4)
-  bonusA = employees[i][2]* 1.09;
+
+for(var i = 0; i < employees.length ; i++){
+    console.log(calculateSTI(employees[i]));
 }
-}
-bonusFinder();
-console.log(bonus);
